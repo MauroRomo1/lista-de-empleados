3c6c5b0ac28e330efdf6c7ced8bdb8be7bf923bf
Mauro Romo <mauroromo.developer@outlook.com>
2023-07-21 18:53:28 -0300

    cambiando la pic de los empleados

--- src/components/AvatarStaff.jsx
+++ src/components/AvatarStaff.jsx
@@ -1,11 +1,13 @@
-const AvatarStaff = ({ fullName, pic }) => {
+import React from "react";
+
+const avatarStaff = ({ fullName, pic }) => {
   return (
     <img
-      src="https://static.vecteezy.com/system/resources/previews/019/896/008/original/male-user-avatar-icon-in-flat-design-style-person-signs-illustration-png.png"
+      src={pic}
       className="img-fluid flex-shrink-0 me-3 avatarStaff"
       alt={fullName}
     />
   );
 };
 
-export default AvatarStaff;
+export default avatarStaff;
