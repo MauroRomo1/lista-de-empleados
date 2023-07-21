ab7404868a5c50caa2700a30d97778f120854a84
Mauro Romo <mauroromo.developer@outlook.com>
2023-07-21 18:12:41 -0300

    añadiendo un icon animado en el h2 del header

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,21 @@ function App() {
   return (
     <>
       <header>
-        <h2 className="text-center mt-3">Lista de empleados</h2>
+        <h2 className="text-center mt-3">
+          <img
+            src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People/Man%20Technologist.png"
+            alt="Man Technologist"
+            className="icon"
+          />
+          Lista de empleados
+        </h2>
+        <hr />
       </header>
       <main>
         <StaffList />
       </main>
       <footer>
+        <hr />
         <h5 className="text-center">&copy; Todos los derechos reservados</h5>
       </footer>
     </>
