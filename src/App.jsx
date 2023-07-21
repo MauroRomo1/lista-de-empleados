96ce758c89b54633012e9882d7091ea2746b531a
Mauro Romo <mauroromo.developer@outlook.com>
2023-07-21 13:00:23 -0300

    limpiando el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h2>header</h2>
+      </header>
+      <main>
+        <h2>Lista de empleados</h2>
+      </main>
+      <footer>
+        <h5>&copy; Todos los derechos reservados</h5>
+      </footer>
+    </>
+  );
+}
+
+export default App;
